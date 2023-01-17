7636bfb8912843fa234d88d1f1ceb4599dbcb783
Iann-rst <iannrst11@gmail.com>
2023-01-17 10:03:33 -0300

    feat: configurando prisma e CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,23 @@
  */
 
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify();
 
-app.get('/', () => {
-  return 'Hello NLW Setup'
+const prisma = new PrismaClient();
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits
 })
 
 app.listen({
   port: 3333
+}).then(() => {
+  console.log('HTTP Server running on port: 3333')
 })
