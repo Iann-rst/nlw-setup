5f243b2c1922a0ef0a559c3122cab23991b134a2
Iann-rst <iannrst11@gmail.com>
2023-01-19 15:33:44 -0300

    feat:Detalhe do dia (hábitos  completos/possíveis)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -29,4 +29,50 @@ export async function appRoutes(app: FastifyInstance) {
       }
     })
   })
+
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day');
+
+
+    // todos hábitos possíveis do dia informado
+    // hábitos que já foram completados
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate()
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+  })
 }
