8c4a7e2837f07377688193bec10441ff3ad0f3d7
Iann-rst <iannrst11@gmail.com>
2023-01-19 13:03:47 -0300

    feat: Rota para criação de novo hábito

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,32 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "./lib/prisma";
+import { z } from 'zod';
+import dayjs from 'dayjs'
 
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const habits = await prisma.habit.findMany();
 
-    return habits
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6))
+    })
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf('day').toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map(weekDay => {
+            return {
+              week_day: weekDay
+            }
+          })
+        }
+      }
+    })
   })
 }
