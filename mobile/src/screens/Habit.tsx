f652193fcc9ede5f04e6c3e815da442d856ad07d
Iann-rst <iannrst11@gmail.com>
2023-01-23 19:43:58 -0300

    feat: Tela de detalhes do hábito do dia

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,9 +1,48 @@
-import { View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { useRoute } from '@react-navigation/native';
+import dayjs from 'dayjs';
+
+import { BackButton } from "../components/BackButton";
+import { ProgressBar } from "../components/ProgressBar";
+import { Checkbox } from "../components/Checkbox";
+
+interface Params {
+  date: string;
+}
+
 
 export function Habit() {
+  const route = useRoute();
+  const { date } = route.params as Params;
+
+  const parsedDate = dayjs(date);
+  const dayOfWeek = parsedDate.format('dddd');
+  const dayAndMonth = parsedDate.format('DD/MM');
+
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
 
+        <Text className="mt-6 text-zinc-400 font-semibold text-base capitalize">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="mt-6 text-white font-extrabold text-3xl capitalize">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={30} />
+
+        <View className="mt-6">
+          <Checkbox title="Beber 2L de água" />
+          <Checkbox title="Caminhar" checked={true} />
+        </View>
+      </ScrollView>
     </View>
   )
 }
