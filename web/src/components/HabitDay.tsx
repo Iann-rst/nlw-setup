12294e90d80e90fd999c9876fda34e8612892a73
Iann-rst <iannrst11@gmail.com>
2023-01-27 17:16:36 -0300

    feat: add animação e focus

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -27,7 +27,7 @@ export function HabitDay({ defaultCompleted = 0, totalHabits = 0, date }: HabitD
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx('w-10 h-10 border-2 rounded-lg', {
+        className={clsx('w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background', {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
           'bg-violet-500 border-violet-400': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-600 border-violet-500': completedPercentage >= 20 && completedPercentage < 40,
