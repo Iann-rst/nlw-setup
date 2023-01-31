3f6c226301aa9e5f2226ff744cf1853df7661dea
Iann-rst <iannrst11@gmail.com>
2023-01-31 11:58:34 -0300

    Feat: Add animação no checkbox

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,4 +1,5 @@
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 
 import { Feather } from '@expo/vector-icons'
 import colors from "tailwindcss/colors";
@@ -19,9 +20,13 @@ export function Checkbox({ title, checked = false, ...rest }: CheckBoxProps) {
     >
       {
         checked ?
-          <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+          <Animated.View
+            className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+            entering={ZoomIn}
+            exiting={ZoomOut}
+          >
             <Feather name="check" size={20} color={colors.white} />
-          </View>
+          </Animated.View>
           :
           <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       }
